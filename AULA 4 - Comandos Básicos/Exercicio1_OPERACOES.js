9e5c8e87a0638578b96f0d511ea587c1036eac81
JVKasper <joaokasper307@gmail.com>
2023-09-29 00:40:44 -0300

    Criação das Aulas de Comandos Básicos

---
+++ AULA 4 - Comandos Básicos/Exercicio1_OPERACOES.js
@@ -0,0 +1,11 @@
+var numero1 = parseInt(prompt("Digite um número"));
+var numero2 = parseInt(prompt("Digite outro número"));
+var soma = numero1 + numero2;
+var subtracao = numero1 - numero2;
+var divisao = numero1 / numero2;
+var multiplicacao = numero1 * numero2;
+
+alert(numero1 + " + " + numero2 + " = "+ soma + "\n" +
+	numero1 + " - " + numero2 + " = "+ subtracao + "\n" +
+	numero1 + " / " + numero2 + " = "+ divisao + "\n" +
+    numero1 + " * " + numero2 + " = "+ multiplicacao);
---
+++ AULA 4 - Comandos Básicos/Exercicio2_ALEATORIO.js
@@ -0,0 +1,4 @@
+// Usando a biblioteca Math.random criar números aleatórios de dois digitos inteiros;
+
+var aleatorio = parseInt(Math.random()*100);
+alert(aleatorio);
